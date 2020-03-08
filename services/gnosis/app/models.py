bafc76515f59270e14b1d521179f7dcffaf39ffb
lambdabyte <52540358+lambdabyte@users.noreply.github.com>
2020-03-08 07:57:49 -0400

    Added some stuff to db

--- services/gnosis/app/models.py
+++ services/gnosis/app/models.py
@@ -1,5 +1,6 @@
 from app import db
 
+# Many to many table
 usersubjects = db.Table('usersubjects',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('subject_id', db.Integer, db.ForeignKey('subject.id'), primary_key=True)
@@ -19,3 +20,7 @@ class User(db.Model):
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject_name = db.Column(db.String(120), index=True, unique=True)
+    color = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Subject {}>'.format(self.subject_name)  
