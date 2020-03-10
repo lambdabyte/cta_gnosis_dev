b07586190d6d3f059e4d7cbb2afcd38d3b8989ed
damthanhhuy <57967842+damthanhhuy@users.noreply.github.com>
2020-03-10 19:13:03 -0400

    change something

--- services/gnosis/app/models.py
+++ services/gnosis/app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Many to many table
 usersubjects = db.Table('usersubjects',
@@ -15,7 +16,13 @@ class User(db.Model):
         backref=db.backref('users', lazy=True))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)    
 
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
