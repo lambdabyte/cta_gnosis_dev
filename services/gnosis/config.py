1c503a127be323038e3868b876116884b75c2700
lambdabyte <52540358+lambdabyte@users.noreply.github.com>
2020-03-11 05:52:35 -0400

    started registration style

--- services/gnosis/config.py
+++ services/gnosis/config.py
@@ -7,6 +7,5 @@ class BaseConfig:
 
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:robertnields2020@cta-gnosis-db-dev.cd1wzcrr5t3s.us-east-1.rds.amazonaws.com'
-    SQLALCHEMY_DATABASE_USER = os.environ.get('POSTGRES_USER')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
