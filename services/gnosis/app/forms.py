494a81288e4d7ae0bbcfa227f66c8db2ac20f959
Cintimd <60855640+Cintimd@users.noreply.github.com>
2020-03-21 22:51:04 -0400

    more user functionality

--- services/gnosis/app/forms.py
+++ services/gnosis/app/forms.py
@@ -26,6 +26,10 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use another email address.')
 
+    def validate_entered_password(self, entered_password):
+        if len(entered_password.data) < 8 or any(str.isdigit(c) for c in entered_password.data) == False: 
+            raise ValidationError('Password must be 8 characters and contain one number.')
+
 class LoginForm(FlaskForm):
     entered_username = StringField('Username', validators=[DataRequired()])
     entered_password = PasswordField('Password', validators=[DataRequired()])
