75956e696be74b90b6d89c7333c194803a4720aa
lambdabyte <52540358+lambdabyte@users.noreply.github.com>
2020-05-12 20:52:46 -0400

    revert screwy

--- services/gnosis/app/clients/__init__.py
+++ services/gnosis/app/clients/__init__.py
@@ -1,1 +1,2 @@
 from .Dynamo_Client import Dynamo_Client
+from .SM_Client import SM_Client
--- services/gnosis/config.py
+++ services/gnosis/config.py
@@ -1,4 +1,5 @@
 import os
+from app.clients import SM_Client
 
 class BaseConfig:
     TESTING = False
@@ -6,14 +7,9 @@ class BaseConfig:
 
 
 class DevelopmentConfig(BaseConfig):
-<<<<<<< HEAD
     dev_sm_db_client = SM_Client('psql-gnosis-dev', 'us-east-1')
     db_secret = dev_sm_db_client.get_secret()
     SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}'.format(db_secret['username'], db_secret['password'], db_secret['host'])
     dev_sm_session_client = SM_Client('session-gnosis-dev', 'us-east-1')
     session_key = dev_sm_session_client.get_secret()
     SECRET_KEY = session_key['session_key']
-=======
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:robertnields2020@cta-gnosis-db-dev.cd1wzcrr5t3s.us-east-1.rds.amazonaws.com'
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
->>>>>>> parent of c086565... cq
